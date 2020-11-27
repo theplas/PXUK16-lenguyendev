5d950f9dbb4bc741dfc100288e21d3407f7815f0
nguyen <kanicepakulate@gmail.com>
2020-11-27 12:05:18 +0700

    Update

--- PXUK16.DAL/BaseRepository.cs
+++ PXUK16.DAL/BaseRepository.cs
@@ -11,7 +11,7 @@ namespace PXUK16.DAL
         protected IDbConnection connect;
         public BaseRepository()
         {
-            connect = new SqlConnection(@"Data Source=DESKTOP-7UEPQID;Initial Catalog=PXUK16DB;User ID=sa;Password=123456789");
+            connect = new SqlConnection(@"Data Source=DESKTOP-CM3G3FU\SQLEXPRESS;Initial Catalog=PXUK16DB;Integrated Security=True");
         }
     }
 }
